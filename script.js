c5955492ddede1066f4a127dacbfa27f7114d204
Md Tuffail Tabish <mdtuffailtabish@gmail.com>
2026-07-29 08:17:55 +0530

    All files are commited!

---
+++ script.js
@@ -0,0 +1,13 @@
+function odd_Even(input) {
+    if(input%2==0){
+        console.log(input, " : Even number");    
+    }
+    else{
+        console.log(input, ": odd Number")
+    }
+}
+
+odd_Even(13);
+odd_Even(12);
+odd_Even(0);
+odd_Even(-73);
